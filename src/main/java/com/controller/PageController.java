182a75d75e39988ad6497a9cfd61b8ef327f266d
agzou <470841118@qq.com>
2017-05-05 11:42:12 +0800

    Third commit

---
+++ src/main/java/com/controller/PageController.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import com.service.PageService;
+import org.ehcache.core.spi.time.SystemTimeSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by AgZou on 2017/5/4.
+ */
+@Controller
+public class PageController {
+    private final PageService pageService;
+
+    @Autowired
+    public PageController(PageService pageService) {
+        this.pageService = pageService;
+    }
+
+    @RequestMapping("/index")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        mv.addObject("page", pageService.getPage());
+        mv.addObject("lights",pageService.getPageHotSales());
+//        mv.addObject("elights",pageService.getPageEndSales());
+        System.out.println(pageService.getPageHotSales().get(0).getPrice());
+        return mv;
+    }
+}
